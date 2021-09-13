78aab5716b568fe16bf6d76dcb91fd4c0f2551c4
riethigSEA <marco-riethig_student2021@wilder.school>
2021-09-13 16:00:01 +0200

    changed from if to while

--- src/CandyCount.java
+++ src/CandyCount.java
@@ -1,24 +1,20 @@
 public class CandyCount {
 
     public static void main(String[] args){
-        double money = 12.4, price = 1.2, pricedifference = 0;
+        double money = 12.4, price = 1.2, pricedifference;
         int candies = 0;
 
         if(money > 0 && price > 0) {
             pricedifference = money - price;
-            if (pricedifference > 0) {
-
+            while(pricedifference > 0){
                 candies++;
                 money = pricedifference;
-            }
-            else{
-                System.out.println("not enough money to buy candy!");
             }
         }
         else{
             System.out.println("you don't have any money!");
         }
-        System.out.println("your currnt money amount: " + money);
+        System.out.println("your current money amount: " + money);
         System.out.println("current amount of candies you own: " + candies);
     }
 }
